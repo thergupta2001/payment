7fe8c94c34b79465c02accb90ea97264b0220115
thergupta2001 <2001.rohangupta@gmail.com>
2025-03-02 19:58:40 +0530

    fetch balance

---
+++ services/balance/src/services/getBalance.service.ts
@@ -0,0 +1,36 @@
+import { CrudService } from "@app/common";
+import Balance from "../models/balance.schema";
+import RedisClient from "@app/common/utils/redis";
+import { Request, Response } from "express";
+import mongoose from "mongoose";
+
+const BalanceService = new CrudService(Balance);
+const redis = RedisClient.getInstance();
+
+export const getBalance = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const cachedBalance = await redis.get(`balance:${id}`);
+
+  const objectId = new mongoose.Types.ObjectId(id);
+
+  if (cachedBalance) {
+    res.status(200).json({ balance: Number(cachedBalance) });
+    return;
+  }
+
+  try {
+    const balance = await BalanceService.findOne({ userId: objectId });
+    if (!balance) {
+      res.status(404).json({ message: "Balance not found" });
+      return;
+    }
+
+    await redis.set(`balance:${id}`, balance.balance, "EX", 3600);
+
+    res.status(200).json({ balance: balance.balance });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error: error });
+  }
+
+  return;
+};
